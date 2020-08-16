ffe76b44c97199f195fd5bdc1d791e4661ab7d45
Paulo Lima <prmlimajr@hotmail.com>
2020-08-16 20:49:07 -0300

    Adicionado criptografia da senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,13 @@
 import connection from '../../database/connection';
+import bcrypt from 'bcryptjs';
 
 class UserController {
   async store(req, res) {
-    const { name, email, password_hash, provider } = req.body;
+    const { name, email, password, provider } = req.body;
     const trx = await connection.transaction();
+    /**
+     * Verifies if the email is already in use.
+     */
     const users = await trx('users').select('users.*');
     const userExists = users.filter((user) => user.email === email);
 
@@ -11,15 +15,22 @@ class UserController {
       return res.status(400).json({ error: 'User already exists' });
     }
 
+    /**
+     * encrypts the password.
+     */
+    const hashedPassword = await bcrypt.hash(password, 8);
+
     const user = {
       name,
       email,
-      password_hash,
+      password_hash: hashedPassword,
       provider: provider || false,
     };
 
+    /**
+     * Gets the user ID for the return.
+     * */
     const insertedIds = await trx('users').insert(user);
-
     const user_id = insertedIds[0];
 
     await trx.commit();
