6e4d396565cba4736c45f1891657fc23ee5b69db
Paulo Lima <prmlimajr@hotmail.com>
2020-08-16 21:54:41 -0300

    Criado sistema de sessão de autentiticação com token

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import connection from '../../database/connection';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+    const trx = await connection.transaction();
+
+    /**
+     * Verifies if the email is already in use.
+     */
+    const users = await trx('users').select('users.*');
+    const userExists = users.filter((user) => user.email === email);
+
+    if (userExists.length === 0) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    const checkPassword = (password) => {
+      return bcrypt.compare(password, userExists[0].password_hash);
+    };
+
+    if (!(await checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = userExists;
+
+    return res.json({
+      user: {
+        id: userExists[0].id,
+        name: userExists[0].name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ba9f9a86c7dc058e926061851af1ec3c',
+  expiresIn: '7d',
+};
