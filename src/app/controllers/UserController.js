82ff03107c1a3c84ec6c3dc705c958e357a9c71a
Paulo Lima <prmlimajr@hotmail.com>
2020-08-17 00:22:02 -0300

    Criada a função update de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
-import connection from '../../database/connection';
 import bcrypt from 'bcryptjs';
+
+import connection from '../../database/connection';
 
 class UserController {
   async store(req, res) {
@@ -42,8 +43,45 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword, password } = req.body;
+    const trx = await connection.transaction();
+    const users = await trx('users').select('users.*');
+    const userExists = users.filter((user) => user.id === req.userId);
+
+    if (email !== userExists[0].email) {
+      // const userExists = users.filter((user) => user.email === email);
+
+      if (userExists.length === 0) {
+        return res.status(400).json({ error: "User doesn't exist" });
+      }
+    }
+
+    const checkPassword = (password) => {
+      return bcrypt.compare(password, userExists[0].password_hash);
+    };
+
+    const hashedPassword = password
+      ? await bcrypt.hash(password, 8)
+      : userExists[0].password_hash;
+
+    if (oldPassword && !(await checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const user = {
+      name: req.body.name || userExists[0].name,
+      email: req.body.email || userExists[0].email,
+      password_hash: hashedPassword,
+    };
+
+    const updated = await trx('users').update(user).where('id', req.userId);
+
+    await trx.commit();
+
+    return res.json({
+      id: req.userId,
+      ...user,
+    });
   }
 }
 
