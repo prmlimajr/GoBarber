ed8f238bf187dd902f39bb9c146c5a25d4ae8a7e
Paulo Lima <prmlimajr@hotmail.com>
2020-08-17 00:53:21 -0300

    Validação com Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,20 @@
 import bcrypt from 'bcryptjs';
+import * as Yup from 'yup';
 
 import connection from '../../database/connection';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const { name, email, password, provider } = req.body;
     const trx = await connection.transaction();
     /**
@@ -43,6 +54,25 @@ class UserController {
   }
 
   async update(req, res) {
+    // const schema = Yup.object().shape({
+    //   name: Yup.string(),
+    //   email: Yup.string().email(),
+    //   oldPassword: Yup.string().min(6),
+    //   password: Yup.string()
+    //     .min(6)
+    //     .when('oldPassword', (oldPassword, field) =>
+    //       oldPassword ? field.required() : field
+    //     ),
+    //   confirmPassword: Yup.string().when('password', (password, field) =>
+    //     password ? field.required().oneOf([Yup.ref('password')]) : field
+    //   ),
+    // });
+
+    // if (!(await schema.isValid(req.body))) {
+    //   console.log(req.body);
+    //   return res.status(400).json({ error: 'Validation failed' });
+    // }
+
     const { email, oldPassword, password } = req.body;
     const trx = await connection.transaction();
     const users = await trx('users').select('users.*');
